badcab7ce922b2f64a1794d60f09fee923a006f4
Shisei Hanai <ruimo.uno@gmail.com>
2025-02-22 17:28:38 +0900

    Can observer log.

--- src/err_log.rs
+++ src/err_log.rs
@@ -1,13 +1,13 @@
-use std::{time::SystemTime, collections::{VecDeque, vec_deque}};
+use std::{cell::RefCell, collections::{vec_deque, VecDeque}, rc::{Rc, Weak}, time::SystemTime};
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum Severity {
     Info,
     Warn,
     Err,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub struct Entry {
     pub time: SystemTime,
     pub severity: Severity,
@@ -36,9 +36,14 @@ impl Entry {
     }
 }
 
+pub trait Observer {
+    fn notify(&mut self, entry: &Entry);
+}
+
 pub struct Logs {
     size: usize,
     logs: VecDeque<Entry>,
+    observers: Vec<Weak<RefCell<dyn Observer>>>,
 }
 
 impl Logs {
@@ -46,7 +51,12 @@ impl Logs {
         Self {
             size,
             logs: VecDeque::with_capacity(size),
+            observers: vec![],
         }
+    }
+    
+    pub fn add_observer(&mut self, observer: Rc<RefCell<dyn Observer>>) {
+        self.observers.push(Rc::downgrade(&observer));
     }
 
     #[inline]
@@ -55,23 +65,30 @@ impl Logs {
             self.logs.remove(0);
         }
     }
+    
+    fn append_log(&mut self, e: Entry) {
+        self.trim();
+        for observer in &self.observers {
+            if let Some(obs) = observer.upgrade() {
+                obs.borrow_mut().notify(&e);
+            }
+        }
+        self.logs.push_back(e);
+    }
 
     pub fn info<S: Into<String>>(&mut self, text: S) {
         let text: String = text.into();
-        self.trim();
-        self.logs.push_back(Entry::info(text));
+        self.append_log(Entry::info(text));
     }
 
     pub fn warn<S: Into<String>>(&mut self, text: S) {
         let text: String = text.into();
-        self.trim();
-        self.logs.push_back(Entry::warn(text));
+        self.append_log(Entry::warn(text));
     }
 
     pub fn err<S: Into<String>>(&mut self, text: S) {
         let text: String = text.into();
-        self.trim();
-        self.logs.push_back(Entry::err(text));
+        self.append_log(Entry::err(text));
     }
 
     /// Oldest first order. You can call rev() to reverse the order.
@@ -100,6 +117,11 @@ macro_rules! err {
 
 #[cfg(test)]
 mod tests {
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    use super::Severity;
+    use super::{Entry, Observer};
     use super::Logs;
 
     #[test]
@@ -122,5 +144,31 @@ mod tests {
         assert_eq!(riter.next().map(|e| e.text.clone()), Some("World 1".to_owned()));
         assert_eq!(riter.next().map(|e| e.text.clone()), Some("Hello".to_owned()));
         assert_eq!(riter.next(), None);
+    }
+    
+    #[test]
+    fn can_observe() {
+        let mut logs = Logs::new(5);
+        struct MyObserver {
+            e: Vec<Entry>,
+        }
+
+        impl Observer for MyObserver {
+            fn notify(&mut self, entry: &Entry) {
+                self.e.push(entry.clone());
+            }
+        }
+        
+        let obs = Rc::new(RefCell::new(MyObserver { e: vec![] }));
+        logs.add_observer(obs.clone());
+
+        info!(logs, "Hello");
+        err!(logs, "World");
+        
+        assert_eq!(obs.borrow().e[0].text, "Hello");
+        assert_eq!(obs.borrow().e[0].severity, Severity::Info);
+
+        assert_eq!(obs.borrow().e[1].text, "World");
+        assert_eq!(obs.borrow().e[1].severity, Severity::Err);
     }
 }
